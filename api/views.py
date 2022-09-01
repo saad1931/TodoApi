c6ef4b60f8b91f5b678e3e898a686c8c962a3b9d
Muhammad Saad <muhammadsaad3119@gmail.com>
2022-09-02 00:36:33 +0500

    django CRUD

---
+++ api/views.py
@@ -0,0 +1,13 @@
+from rest_framework import generics
+from .models import Todo
+from .serializers import TodoSerializer
+
+class TodoGetCreate(generics.ListCreateAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+
+
+
+class TodoUpdateDelete(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
